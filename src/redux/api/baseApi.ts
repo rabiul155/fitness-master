9d7fdc44d38a08901a0ba383b7799a7f6bdd4001
rabiul155 <siyamict@gamil.com>
2024-07-25 12:38:20 +0600

    title change

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://fitness-master-server.vercel.app/api/",
+  }),
   endpoints: () => ({}),
   tagTypes: ["cart", "product"],
 });
